8599507e3fb7a0e2c5e23812c818324ae4ad3b0a
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-11 14:46:20 +0100

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,29 +1,30 @@
-const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../util/caseNumber.js');
-const {parseUser} = require('../util/parseUser.js');
-const settings = require('../settings.json');
-exports.run = async (client, message, args) => {
-  const user = message.mentions.users.first();
-  parseUser(message, user);
-  const modlog = client.channels.find('name', 'mod-log');
-  const caseNum = await caseNumber(client, modlog);
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
   if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the kick.');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to kick them.').catch(console.error);
 
-  // message.guild.member(user).kick();
+  if (!message.guild.member(user).kickable) return message.reply('I cannot kick that member');
+  message.guild.member(user).kick();
 
-  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
-  const embed = new RichEmbed()
-  .setColor(0x00AE86)
-  .setTimestamp()
-  .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-  .setFooter(`Case ${caseNum}`);
-  return client.channels.get(modlog.id).send({embed});
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .addField('Action:', 'kick')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
 };
 
 exports.conf = {
+  enabled: true,
+  guildOnly: false,
   aliases: [],
-  permLevel: 2
+  permLevel: 0
 };
 
 exports.help = {
