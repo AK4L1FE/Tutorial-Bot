e0e20a72747c088fce5889a16cf0c189f3eb886b
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-11 14:37:37 +0100

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,4 @@
 module.exports = member => {
   const guild = member.guild;
-  guild.defaultChannel.send(`Please welcome ${member.user.username} to the server!`);
+  guild.defaultChannel.send(`Welcome to the server ${member.user.username}!`);
 };
