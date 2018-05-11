35de7b13001d9d83baba6015796a27bb0d7100d8
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-11 14:38:24 +0100

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,4 @@
 module.exports = member => {
   const guild = member.guild;
-  guild.defaultChannel.send(`Please say goodbye to ${member.user.username} we will miss you!`);
+  guild.defaultChannel.send(`Goodbye, ${member.user.username}, we will miss you!`);
 };
