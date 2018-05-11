fd8542c051fc972aebce8ba76dd4873015ced2e4
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-11 14:50:01 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,29 +1,26 @@
-const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../util/caseNumber.js');
-const {parseUser} = require('../util/parseUser.js');
-const settings = require('../settings.json');
-exports.run = async (client, message, args) => {
-  const user = message.mentions.users.first();
-  parseUser(message, user);
-  const modlog = client.channels.find('name', 'mod-log');
-  const caseNum = await caseNumber(client, modlog);
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
   if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
-
-  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
-  const embed = new RichEmbed()
+  const embed = new Discord.RichEmbed()
   .setColor(0x00AE86)
   .setTimestamp()
-  .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-  .setFooter(`Case ${caseNum}`);
-  return client.channels.get(modlog.id).send({embed});
+  .addField('Action:', 'Warning')
+  .addField('User:', `${user.username}#${user.discriminator}`)
+  .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+  .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
   aliases: [],
-  permLevel: 2
+  permLevel: 0
 };
 
 exports.help = {
