5219077f132a3a9bee713a45d3dde2bba051d125
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-11 14:47:32 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,33 +1,30 @@
-const {RichEmbed} = require('discord.js');
-const {caseNumber} = require('../util/caseNumber.js');
-const {parseUser} = require('../util/parseUser.js');
-const settings = require('../settings.json');
-exports.run = async (client, message, args) => {
-  const user = message.mentions.users.first();
-  parseUser(message, user);
-  const modlog = client.channels.find('name', 'mod-log');
-  const caseNum = await caseNumber(client, modlog);
-  const muteRole = client.guilds.get(message.guild.id).roles.find('name', 'muted');
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
+  let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'muted');
   if (!modlog) return message.reply('I cannot find a mod-log channel').catch(console.error);
   if (!muteRole) return message.reply('I cannot find a mute role').catch(console.error);
+  if (reason.length < 1) return message.reply('You must supply a reason for the mute.').catch(console.error);
   if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
-  const reason = args.splice(1, args.length).join(' ') || `Awaiting moderator's input. Use ${settings.prefix}reason ${caseNum} <reason>.`;
-
-  const embed = new RichEmbed()
+  const embed = new Discord.RichEmbed()
     .setColor(0x00AE86)
     .setTimestamp()
-    .setDescription(`**Action:** Un/mute\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`)
-    .setFooter(`Case ${caseNum}`);
+    .addField('Action:', 'Un/Mute')
+    .addField('User:', `${user.username}#${user.discriminator} (${user.id})`)
+    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`)
+    .addField('Reason', reason);
 
   if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the correct permissions.').catch(console.error);
 
   if (message.guild.member(user).roles.has(muteRole.id)) {
     message.guild.member(user).removeRole(muteRole).then(() => {
-      client.channels.get(modlog.id).send({embed}).catch(console.error);
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
     });
   } else {
     message.guild.member(user).addRole(muteRole).then(() => {
-      client.channels.get(modlog.id).send({embed}).catch(console.error);
+      client.channels.get(modlog.id).sendEmbed(embed).catch(console.error);
     });
   }
 
@@ -36,8 +33,8 @@ exports.run = async (client, message, args) => {
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['unmute'],
-  permLevel: 2
+  aliases: [],
+  permLevel: 0
 };
 
 exports.help = {
