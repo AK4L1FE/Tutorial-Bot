16f72eae84f12e6998a259e2c331fec4ec080417
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-13 10:30:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 const chalk = require('chalk');
