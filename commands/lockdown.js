e8e7179a6805bde3cd192bb09123406ac3e226f5
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-13 14:51:04 +0100

    Update lockdown.js

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args) => {
 
   if (validUnlocks.includes(time)) {
     message.channel.overwritePermissions(message.guild.id, {
-      SEND_MESSAGES: null
+      SEND_MESSAGES: false
     }).then(() => {
       message.channel.send('Lockdown lifted.');
       clearTimeout(client.lockit[message.channel.id]);
@@ -23,7 +23,7 @@ exports.run = (client, message, args) => {
 
         client.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: null
+            SEND_MESSAGES: false
           }).then(message.channel.send('Lockdown lifted.')).catch(console.error);
           delete client.lockit[message.channel.id];
         }, ms(time));
