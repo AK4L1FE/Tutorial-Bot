e16a60b2dd32d256659e39032ce6f304fc9622ea
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-13 12:48:51 +0100

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,4 @@
-const Discord = require('discord.js');
 const chalk = require('chalk');
-const bot = new Discord.Client();
- bot.on("ready", async () => {
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-     bot.user.setActivity(`${bot.user.username} is online on ${bot.guilds.size} servers!`, {type: 2});
-     
-     });
+module.exports = client => { // eslint-disable-line no-unused-vars
+  console.log(chalk.bgGreen.black('I\'m Online'));
+};
