d4cf5a895e4f3586f16441815cb56723bc67537a
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-13 12:32:26 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,4 +3,4 @@ module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
      bot.user.setActivity(`${bot.user.username} is online on ${bot.guilds.size} servers!`, {type: 2});
      
-     });
+     };
