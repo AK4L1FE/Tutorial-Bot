4df3d417b6581a78b259f980d7d0c41f38a63eb6
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-13 12:39:49 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const chalk = require('chalk');
 const bot = new Discord.Client();
 module.exports = client => { // eslint-disable-line no-unused-vars
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+    console.log(`Moderator is online on ${bot.guilds.size} servers!`);
      bot.user.setActivity(`${bot.user.username} is online on ${bot.guilds.size} servers!`, {type: 2});
 
      };
