64ba84220222845bb522fc2fba8cf60bdec495b8
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-14 02:04:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -65,40 +65,21 @@ bot.on('guildMemberRemove', member => {
     console.log("Leave Message Sent")
 });
 
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./commands/', (err, files) => {
-  if (err) console.error(err);
-  log(`Loading a total of ${files.length} commands.`);
-  files.forEach(f => {
-    const props = require(`./commands/${f}`);
-    log(`Loading Command: ${props.help.name}. 👌`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.name);
-    });
-  });
-});
+fs.readdir("./commands/", (err, files) => {
+    if(err) console.log(err);
 
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./commands/${command}`)];
-      const cmd = require(`./commands/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e) {
-      reject(e);
-    }
-  });
-};
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if(jsfile.length <= 0){
+        console.log("couldn't find commands");
+        return;
+    };
+
+    jsfile.forEach((f, i) =>{
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded`);
+        bot.commands.set(props.help.name, props);
+    });
+});
 
 client.elevation = message => {
   /* This function should resolve to an ELEVATION level which
