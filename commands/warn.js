e6e4a3df9d85122a859bb062af05f671f2b753bb
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-14 02:23:55 +0100

    Add files via upload

--- commands/warn.js
+++ commands/warn.js
@@ -1,68 +1,30 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
+  const embed = new Discord.RichEmbed()
+  .setColor(0x00AE86)
+  .setTimestamp()
+  .addField('Action:', 'Warning')
+  .addField('User:', `${user.username}#${user.discriminator}`)
+  .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
+  .addField('Reason', reason);
+  return client.channels.get(modlog.id).sendEmbed(embed);
+};
 
-module.exports.run = async (bot, message, args) => {
-
-  //!warn @daeshan <reason>
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to do that.");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Sorry, but we can't find that user.");
-  let reason = args.join(" ").slice(22);
-
-  if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-  };
-
-  warns[wUser.id].warns++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-    if (err) console.log(err)
-  });
-
-  message.channel.send(`${wUser} has been warned. He has been warned ${warns[wUser.id].warns} time(s).`);
-
-  if(warns[wUser.id].warns == 3){
-    let muterole = message.guild.roles.find(`name`, "mute");
-    if(!muterole){
-      try{
-        muterole = await message.guild.createRole({
-          name: "muted",
-          color: "#000000",
-          permissions:[]
-        })
-        message.guild.channels.forEach(async (channel, id) => {
-          await channel.overwritePermissions(muterole, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false,
-            READ_MESSAGES: false,
-            VOICE_CONNECT: false,
-            CREATE_INSTANT_INVITE: false,
-            CHANGE_NICKNAME: false
-          });
-        })
-      }catch(e){
-        console.log(e.stack);
-      };
-    };
-    
-    if(!muterole) return message.reply(`${wUser} has been temporarily muted.`);
-
-    let mutetime = "300s";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`<@${wUser.id}> has been temporarily muted for 5 minutes.`);
-
-    setTimeout(function(){
-      wUser.removeRole(muterole.id)
-      message.reply(`<@${wUser.id}> has been unmuted.`)
-    }, ms(mutetime))
-  }
-
-}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
 
 exports.help = {
   name: 'warn',
   description: 'Issues a warning to the mentioned user.',
   usage: 'warn [mention] [reason]'
-};
+}
