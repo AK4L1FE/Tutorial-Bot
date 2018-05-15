58556a5c8d906bc2aaa1ba3c853158eb4c5df581
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-15 19:45:59 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,14 +3,14 @@ exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-    return message.reply('check your DMs for the command list');
+    message.reply('check your DMs for the command list');
     message.author.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}`, {code:'asciidoc'});
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
       command = client.commands.get(command);
       message.author.send(`= ${command.help.name} = \n${command.help.description}\nusage::${command.help.usage}`, {code:'asciidoc'});
-      message.reply('check your DMs for the command you have mentioned above.');
+      return message.reply('check your DMs for the command you have mentioned above.');
     }
   }
 };
