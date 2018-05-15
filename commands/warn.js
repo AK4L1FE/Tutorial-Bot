20a602259dc18aad4af612fdf2b85fd27483c740
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-16 00:54:01 +0100

    Add files via upload

--- commands/warn.js
+++ commands/warn.js
@@ -16,41 +16,7 @@ exports.run = (client, message, args) => {
     let incidentchannel = message.guild.channels.find(`name`, "mod-log");
     if(!incidentchannel) return message.channel.send("Can't find a mod-log channel.");
 
-    incidentchannel.send(embed);const Discord = require('discord.js');
-exports.run = (client, message, args) => {
-  let reason = args.slice(1).join(' ');
-  let user = message.mentions.users.first();
-
-
-  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
-  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
-  if (message.mentions.users.size = 1) return user.send(`You were warned for this reason: ${reason}`);
-  const embed = new Discord.RichEmbed()
-  .setColor(0x00AE86)
-  .setTimestamp()
-  .addField('Action:', 'Warning')
-  .addField('User:', `@${user.username}#${user.discriminator}`)
-  .addField('Moderator:', `${message.author.username}#${message.author.discriminator}`)
-  .addField('Reason', reason);
-
-    let incidentchannel = message.guild.channels.find(`name`, "mod-log");
-    if(!incidentchannel) return message.channel.send("Can't find a mod-log channel.");
-
     incidentchannel.send(embed);
-};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'warn',
-  description: 'Issues a warning to the mentioned user, needing to have a mod-log channel.',
-  usage: 'warn [mention] [reason]'
-}
 };
 
 exports.conf = {
