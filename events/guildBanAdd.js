aeea6af4a9f95933663c5f796457491cbfd8d407
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-16 22:33:18 +0100

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-module.exports = (client, message, user, args) => {
+module.exports = (guild, client, message, user, args) => {
   let welcomechannel = guild.channels.find(`name`, "welcome_leave");
   welcomechannel.send(`${user.username} was just banned!`);
 };
