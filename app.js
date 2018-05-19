3e15cccc3c73ae4f801385e62cd710afa80adf50
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-19 22:06:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const newUsers = [];
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on('guildMemberAdd', (member, guild, name, server) => {
