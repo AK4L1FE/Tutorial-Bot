fbd3a1223910bb67c4c5c9b064aa4c26b335e908
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-19 19:09:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const bot = new Discord.Client();
 
 
 const newUsers = [];
@@ -8,27 +9,16 @@ client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+client.on('guildMemberAdd', (member, guild) => {
+  const channel = member.guild.channels.find('name', 'welcome');
+  if (!channel) return;
+  channel.send(`Welcome to ${guild.name}, ${member}`);
 });
 
-client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-  newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 1) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find("name", "welcome").send("Welcome to the server!!!");
-    newUsers[guild.id].clear();
-  }
-});
-
-client.on("guildMemberRemove", (member) => {
-  const guild = member.guild;
-  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+client.on('guildMemberRemove', (member, guild) => {
+  const channel = member.guild.channels.find('name', 'welcome');
+  if (!channel) return;
+  channel.send(`Goodbye, ${member}, we will miss you`);
 });
 
 client.login('NDQyNzQ0MjIzMjAxMDk5Nzc3.DeGOKA.yFpFReKWxCdIGEY-CD0rnyG2h7o');
