320d294933c3511528c708c4d88dbe7058a3dc5c
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-19 10:41:24 +0100

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -4,5 +4,4 @@ module.exports = async (client, message) => {
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
   client.on('message', reqEvent('message'));
-  client.on('guildBanAdd', reqEvent('guildBanAdd'));
 };
