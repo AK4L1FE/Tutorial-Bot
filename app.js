4becbebe766c58a687e88318e788842a98c21ed4
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-19 18:23:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,116 +1,34 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const bot = new Discord.Client();
-const ddiff = require('return-deep-diff');
-client.on('ready', () => {
-  console.log('I\'m Online\nI\'m Online');
+
+
+const newUsers = [];
+
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
-// client.on('',''=>{});
-
- bot.on("ready", async (user, bot, guild) => {
-    console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
-     bot.user.setActivity(`${bot.user.username} is online on ${bot.guilds.size} servers!`, {type: 2});
-	 
-     });
-
-bot.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'welcome-leave');
-    let memberavatar = member.user.avatarURL
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':bust_in_silhouette: | name : ', `${member}`)
-        .addField(':microphone2: | Welcome!', `Welcome to the server, ${member}`)
-        .addField(':id: | User :', "**[" + `${member.id}` + "]**")
-        .addField(':family_mwgb: | You are the member', `${member.guild.memberCount}`)
-        .addField("Name", `<@` + `${member.id}` + `>`, true)
-        .addField('Server', `${member.guild.name}`, true )
-        .setFooter(`**${member.guild.name}**`)
-        .setTimestamp()
-
-        channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberRemove', member => {
-    let channel = member.guild.channels.find('name', 'welcome-leave');
-    let memberavatar = member.user.avatarURL
-        if (!channel) return;
-        let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField('Name:', `${member}`)
-        .addField('Has Let the Server', ';(')
-        .addField('Bye Bye :(', 'We will all miss you!')
-        .addField('The server now as', `${member.guild.memberCount}` + " members")
-        .setFooter(`**${member.guild.name}`)
-        .setTimestamp()
-
-        channel.sendEmbed(embed);
-});
-
-var prefix = "~"
-client.on('message', message => {
-  let args = message.content.split(' ').slice(1);
-  var result = args.join(' ');
-
-  if (!message.content.startsWith(prefix)) return;
-  if (message.author.bot) return;
-
-  if (message.content.startsWith(prefix + 'join')) {
-		let voiceChan = message.member.voiceChannel;
-		if (!voiceChan || voiceChan.type !== 'voice') {
-			message.channel.sendMessage('No').catch(error => message.channel.sendMessage(error));
-		} else if (message.guild.voiceConnection) {
-			message.channel.sendMessage('I\'m already in a voice channel');
-		} else {
-			message.channel.sendMessage('Joining...').then(() => {
-				voiceChan.join().then(() => {
-					message.channel.sendMessage('Joined successfully.').catch(error => message.channel.sendMessage(error));
-				}).catch(error => message.channel.sendMessage(error));
-			}).catch(error => message.channel.sendMessage(error));
-		}
-	} else
-
-	if (message.content.startsWith(prefix + 'leave')) {
-		let voiceChan = message.member.voiceChannel;
-		if (!voiceChan) {
-			message.channel.sendMessage('I am not in a voice channel');
-		} else {
-			message.channel.sendMessage('Leaving...').then(() => {
-				voiceChan.leave();
-			}).catch(error => message.channel.sendMessage(error));
-		}
-	}
-
-
-  if (message.content.startsWith(prefix + 'ping')) {
-    message.channel.sendMessage(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
-  } else
-
-  if (message.content.startsWith(prefix + 'send')) {
-    client.channels.get('245491978601627648').sendMessage('Hello to the second channel!');
-  } else
-
-  if (message.content.startsWith(prefix + 'setgame')) {
-    if (!result) {
-      result = null;
-    }
-    client.user.setGame(result);
-  } else
-
-  if (message.content.startsWith(prefix + 'setstatus')) {
-    if (!result) {
-      result = 'online';
-    }
-    client.user.setStatus(result);
-  } else
-
-  if (message.content.startsWith(prefix + 'foo')) {
-    message.channel.sendMessage('bar');
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
   }
 });
 
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find("name", "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
 
 client.login('NDQyNzQ0MjIzMjAxMDk5Nzc3.DeGOKA.yFpFReKWxCdIGEY-CD0rnyG2h7o');
