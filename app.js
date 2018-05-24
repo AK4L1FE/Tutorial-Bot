10245a5b86444f1597bc6acaa7f69e35dc8a803a
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-24 23:17:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const client Discord.Client();
+const fs = require('fs');
+const db = require('quick.db');
 const OwnerID = "309081957604786176";
 
 const prefix = "gt!"
@@ -15,16 +17,16 @@ client.on("ready", async () => {
   
 });
 
-client.on('guildMemberAdd', (member, guild, name, server) => {
-  const cchannel = member.guild.channels.find('name', 'welcome');
-  if (!cchannel) return;
-  cchannel.sendMessage(`${member}, welcome to ${member.guild.name}!!! :laughing:`);
+client.on('guildMemberAdd', (member, message, guild, name, server) => {
+  const welcomechannel = require("setchannel.js");
+  if (!welcomechannel) return;
+  welcomechannel.sendMessage(`${member}, welcome to ${member.guild.name}!!! :laughing:`);
 });
 
 client.on('guildMemberRemove', (member, guild, name) => {
-  const cchannel = member.guild.channels.find('name', 'welcome');
-  if (!cchannel) return;
-  cchannel.sendMessage(`Goodbye, ${member}, we will miss you. :slight_frown:`);
+  const welcomechannel = require("setchannel.js");
+  if (!welcomechannel) return;
+  welcomechannel.sendMessage(`Goodbye, ${member}, we will miss you. :slight_frown:`);
 });
 
 client.login('NDQ0Mzk0NjIzNDE5NTQ3NjQ4.DeIusw.wHLCw0fPFwg9xjBmbs4BGd769UI');
