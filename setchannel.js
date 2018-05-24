8fee5e1be2ea5b7fc03448b8b61dc51b7595bed9
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-24 23:35:52 +0100

    Update setchannel.js

--- setchannel.js
+++ setchannel.js
@@ -1,5 +1,4 @@
 const discord = require('discord.js');
-const db = require('quick.db');
 const bot = new discord.Client();
 
 bot.on("message", async (message, args, func, bot) => {
@@ -13,16 +12,12 @@ let args = messageArray.slice(1);
 
  if(cmd === `${prefix}setchannel`){
      
-     
     if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires the Owner role**', 120000); // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
     if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return func.embed(message.channel, '**Please mention a channel**\n > *~setChannel #channel*');
 
     let newChannel;
     if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; // If they wrote the word none, it sets newChannel as empty.
     else newChannel = message.mentions.channels.first().id; // If they actually mentioned a channel, it will set newChannel as that.
-
-    db.updateText(`messageChannel_${message.guild.id}`, newChannel).then(i => {
-        func.embed(message.channel, `**Successfully updated logging channel to ${message.mentions.channels.first()}**`);
     })
 
 }
