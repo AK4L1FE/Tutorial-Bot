b5b1387cc73621f3be8b34005baf55459e9bd0ac
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-24 22:25:56 +0100

    Create announce.js

---
+++ commands/announce.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+ let say_msg = args.slice().join(" ");
+message.channel.send(`@everyone`);
+message.channel.send(say_msg);
+  message.delete();
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'announce',
+  description: 'Mentions everyone and says what you type in.',
+  usage: 'announce {message}'
+}
