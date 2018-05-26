efc9767109069e35e3951dbc30d892fa194796f9
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-26 22:31:34 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const chalk = require('chalk');
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
   client.user.setStatus('dnd');
-  client.user.setPresence({ game: { name: `${client.user.username} is online on ${client.guilds.size} servers!`, type: 0} });
+  client.user.setPresence({ stream: { stream: `;help | ${client.user.username} is online on ${client.guilds.size} servers!`, type: 0} });
   
 };
