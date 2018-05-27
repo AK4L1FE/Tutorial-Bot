7d5ea8a6fc98322067a72b2624fc5da766f381fa
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-27 18:10:14 +0100

    Create setusername.js

---
+++ commands/setusername.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+exports.run = (client, message, args) => {
+ let say_msg = args.slice().join(" ");
+
+ client.setUsername(say_msg);
+
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'setusername',
+  description: 'Sets the bot's username,can only be used by the owner.',
+  usage: 'setusername {message}'
