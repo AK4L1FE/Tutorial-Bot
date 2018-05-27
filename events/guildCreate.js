2fd4c12d4e0f3c1ed536cf295a51d971284a4248
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-28 00:52:32 +0100

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,5 +2,5 @@ module.exports = async client => {
   console.log('I joined a new server!!!');
   const bchannel = member.guild.channels.find(`name`, "general");
   if (!bchannel) return;
-  bchannel.sendMessage(`Hi, I\'m Z-Bot and I came here to help, join my support server if you want more help: https://discord.gg/U3hbwrt`);
+  bchannel.sendMessage(`Hi, I\'m Z-Mod and I came here to help, join my support server if you want more help: https://discord.gg/U3hbwrt`);
 };
