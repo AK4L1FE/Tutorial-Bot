6e47fe0916caa083433a6f9a693b0a4e9f9671a7
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-27 20:05:47 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const chalk = require('chalk');
 module.exports = async client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
   client.user.setStatus('dnd');
-  client.user.setPresence({ game: { name: `;help | ${client.user.username} is online on ${client.guilds.size} servers!`, name: `https://www.twitch.tv/ak4l1fe`, type: 1} });
+  client.user.setPresence({ game: { name: `;help | ${client.user.username} is online on ${client.guilds.size} servers!`, type: 1} });
   
 };
