29d4d24296489bcf64782bf9ac63732a0b64ad26
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-28 00:21:17 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, guild, message, args, name) => {
-message.reply(`Name: ${guild.name}n\Owner: ${guild.ownerID}n\Icon: ${guild.icon}`);
+return message.reply(`Name: ${guild.name}n\Owner: ${guild.ownerID}n\Icon: ${guild.icon}`);
 };
 
 exports.conf = {
