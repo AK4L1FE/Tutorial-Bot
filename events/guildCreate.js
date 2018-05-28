81843e4ca4339f93eaa5da142b75256e2aaced9b
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-28 01:06:01 +0100

    Delete guildCreate.js

--- events/guildCreate.js
+++
@@ -1,7 +0,0 @@
-var guild = this.client.guilds.get(this.client.channelGuildMap[packet.d.id])
-module.exports = async (client, guild, member) => {
-  console.log('I joined a new server!!!');
-  const bchannel = member.guild.channels.find(`name`, "general");
-  if (!bchannel) return;
-  bchannel.sendMessage(`Hi, I\'m Z-Mod and I came here to help, join my support server if you want more help: https://discord.gg/U3hbwrt`);
-};
