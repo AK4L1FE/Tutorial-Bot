6383c6ab7fc9262bd44253d9bdf36bdc70a3026a
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-29 22:55:05 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 exports.run = (client, message, args, bot) => {
-message.channel.send(`**Z-mod's Stats**\n\n**Users:**\n${bot.users.size}\n\n**Channels:**\n${bot.channels.size}\n\n**Servers:**\n${bot.guilds.size}`);
+message.channel.send(`**Z-mod's Stats**\n\n**Users:**\n${bot.users.size}\n\n**Channels:**\n${bot.channels.size}\n\n**Servers:**\n${bot.servers.size}`);
 }
 
 exports.conf = {
