1dbbd71e7a3693eea4af239e3f9d230ccd6c386f
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-29 23:38:06 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-exports.run = (client, guild, message, args, channel) => {
+exports.run = (client, message, guild, args, channel) => {
 message.channel.send(`**${guild.name}** ${guild.icon}\n\n**Members and Bots:**\n${guild.memberCount}\n\n**When it was created:**\n${guild.createdAt}\n\n**Owner:**\n@${guild.ownerID}\n\n**Verification level:**\n${guild.verificationLevel}\n\n**Region:**\n${guild.region}`);
 }
 
