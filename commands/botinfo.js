0ea9760c98108841f0c53627edbe3fab4ea0094b
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-31 02:33:06 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,3 +1,16 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args, guild) => {
 message.channel.send(`**Z-Mod**\n${clientUser..displayAvatarURL}\n\n**Version**\n1.0.2\n\n**Created at:**\n${clientUser.createdAt}\n\n**Library:**\ndiscord.js\n\n**Creator:**\n@AK4L1FE#4246\n\n**Invite to Support:**\nhttps://discord.gg/HsawkcD\n\n**Donate to my creator:**\nhttps://www.patreon.com/user?u=11509428`);
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'botinfo',
+  description: 'Displays the bot\'s information.',
+  usage: 'botinfo'
+}
