8bf750b795989b515a876bfe80d28be6ccc9f94d
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-31 03:33:18 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-exports.run = async (client, clientUser, message, args, guild) => {
+exports.run = async (clientUser, message, args) => {
 message.channel.send(`**Z-Mod**\n${clientUser.displayAvatarURL}\n\n**Version**\n1.0.2\n\n**Created at:**\n${clientUser.createdAt}\n\n**Library:**\ndiscord.js\n\n**Creator:**\n@AK4L1FE#4246\n\n**Invite to Support:**\nhttps://discord.gg/HsawkcD\n\n**Donate to my creator:**\nhttps://www.patreon.com/user?u=11509428`);
 };
 
