79c40a56e87cd027f870c7980ce97386a7682303
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-05-31 20:15:54 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -5,8 +5,8 @@ exports.run = async (client, message, args) => {
   let toMute = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if (!toMute) return message.channel.sendMessage("You did not specify a user mention or ID!");
   
-  if (toMute.id === message.author.id) return message.chennel.sendMessage("You cannot mute yourself.");
-  if (toMute.highestRole.position >= message.member.highestRole.position) return message.channel.send("You cannot mute a member who is higher or has the same role as you.");
+  if (toMute.id === message.author.id) return message.channel.sendMessage("You cannot mute yourself.");
+  if (toMute.highestRole.position >= message.member.highestRole.position) return message.channel.sendMessage("You cannot mute a member who is higher or has the same role as you.");
   
   let rolek = message.guild.roles.find("name", "Muted");
   if (!rolek) {
