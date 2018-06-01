175390a2b5e7dcbd1cfbbd766da7b84531eb147f
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-01 20:15:37 +0100

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-exports.run = async (client, message, args, superagent) => {
+const superagent = require('superagent');
+exports.run = async (client, message, args) => {
 const { request} = await superagent
     .get('https://api.ksoft.si/meme/random-meme')
     .set('Authorization', 'Token 90c394ab0eddb46cf8e7b9ced143f792cb254a89')
