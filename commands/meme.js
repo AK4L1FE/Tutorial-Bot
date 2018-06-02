22511dfa6b3f336bdc36f88e823b46aeb13aaac0
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-02 14:56:23 +0100

    Create meme.js

---
+++ commands/meme.js
@@ -0,0 +1,22 @@
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  meme(function(data) {
+  const embed = new Discord.RichEmbed()
+  .setTitle(data.title[0])
+  .setColor("#7d1616")
+  .setImage(data.url[0])
+  message.delete().catch(O_o => {});
+  message.channel.send({embed});
+  })};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["meme", "memez"],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "meme",
+  description: "Memez 4 life",
+  usage: "meme"
+};
