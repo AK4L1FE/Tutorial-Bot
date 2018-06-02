2645579a25562ee24e58c21919d99b36c8ce8063
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-02 18:27:57 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,37 +1,44 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const YTDL = require("ytdl-core");
-const ffmpeg = require('ffmpeg');
-exports.run = (client, message, args, servers) => {
-  if (!args[0]) {
-    message.channel.send(`Please provide a link.`);
-    return;
-  };
-  
-  if (!message.member.voiceChannel) {
-    message.channel.send(`You must be a voice channel.`);
-    return;
-  };
-  
-  if (!servers[message.guild.id]) servers[message.guild.id] = {
-    queue: []
-  };
-  
+var servers = {};
+function play (connection, message) {
   var server = servers[message.guild.id];
-  
-  if (!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-    play(connection, message);
-  });
-};
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 2
-};
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+  server.queue.shift();
+
+  server.dispatcher.on("end", function()  {
+      if (server.queue[1]) play(connection, message);
+      else connection.disconnect();
+
+});
+}
+exports.run = async (client, message, args) => {
+
+if  (!args[0]) {
+message.channel.sendMessage("Please Provide a Link");
+return;
+}
+if (!message.member.voiceChannel) {
+    message.channel.sendMessage("You must be in a voice channel");
+    return;
+  }
+  if(!servers[message.guild.id]) servers[message.guild.id] = {
+      queue: [ ]
+    };
+
+    var server = servers[message.guild.id];
+
+    server.queue.push(args[0]);
+
+    if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection)  {
+      play(connection, message);
+    });
+  }
 
 exports.help = {
-  name: 'play',
+  name: "play",
   description: 'Plays music.',
   usage: 'play {message}'
 }
