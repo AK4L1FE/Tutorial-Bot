62a3f11a0256ae3dedd5a00cf7fe9243615b3298
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-02 19:48:02 +0100

    Create chat.js

---
+++ commands/chat.js
@@ -0,0 +1,26 @@
+exports.run = (client, message, args) => {
+    const cleverbot = require("cleverbot.io");
+    const clever = new cleverbot("XinPY3bIluK693Ly", "iA5kxqieNGXlLYauuaehvFujXRDpGWGA");
+    clever.setNick("zmodmvp");
+    clever.create(function(err, session) {
+        clever.ask(args.join(' '), function(err, res) {
+            message.channel.send(res);
+        });
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'chat',
+  description: 'CleverBot.',
+  usage: 'chat {message}'
+};
+
+
+
