abcd3a2b6a6767737ee66747e1077ce2f5b0b25d
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-03 16:18:57 +0100

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+var servers = {};
+function play (connection, message) {
+  var server = servers[message.guild.id];
+
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+  server.queue.shift();
+
+  server.dispatcher.on("end", function()  {
+      if (server.queue[1]) play(connection, message);
+      else connection.disconnect();
+
+});
+}
+
+exports.run = async (client, message, args) => {
+    var server = servers[message.guild.id];
+
+    if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+    return;
+        }
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'stop',
+  description: 'Stops music.',
+  usage: 'stop'
+}
