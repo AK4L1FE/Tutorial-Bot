df8488cc9d9918abe97e400419ed937dda6f05e7
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-03 20:33:11 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,8 @@ module.exports = async client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
   client.user.setStatus('dnd');
   setInterval(function () {
-    client.user.setGame(`;;help | ${client.users.size} Users | ${client.guilds.size} Servers`, 'https://www.twitch.tv/ak4l1fe'); 
-    client.user.setGame(`;;help | https://z-mod.glitch.me/`, 'https://www.twitch.tv/ak4l1fe');
+    client.user.setGame(`;help | ${client.users.size} Users | ${client.guilds.size} Servers`, 'https://www.twitch.tv/ak4l1fe'); 
+    client.user.setGame(`;help | https://z-mod.glitch.me/`, 'https://www.twitch.tv/ak4l1fe');
 //random status'es in here
   }, [5000]);
  
