51eb53a28d08df76dcfa1000c7d60ebeb710a1df
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-04 19:11:47 +0100

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+let user = message.mentions.users.first() || message.author;
+
+
+exports.run = async (client, message, args, level) => {
+const embed = new Discord.RichEmbed()
+  .setTitle(`${user.name}`)
+  .setColor("#7d1616")
+  .setImage(user.displayAvatarURL)
+  message.delete().catch(O_o => {});
+  message.channel.send({embed});
+  })};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["av"],
+  permLevel: 0
+};
+
+exports.help = {
+  name: "avatar",
+  description: "Displays your avatar or a mentioned user's avatar.",
+  usage: "avatar {mention}"
+};
