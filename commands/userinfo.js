e7c76ad524106f1497c9a5728816172c7cc84372
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-05 22:28:02 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -5,10 +5,10 @@ const ms = require("ms");
 exports.run = async (bot, message, args) => {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
+  const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
   if(!user) return message.reply("I Couldn't find him.");
   let warnlevel = warns[user.id].warns;
-  const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
   const embed = new Discord.RichEmbed()
 
   .setTitle(`${user.username}`)
