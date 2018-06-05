c58d4a826caf865de611429c8a536c5a190f5e4f
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-05 22:46:20 +0100

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 const ms = require("ms");
 
-exports.run = async (bot, message, args, fs) => {
-
+exports.run = async (bot, message, args) => {
+  var fs = require('fs')
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
   let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
   let user = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
@@ -13,6 +13,8 @@ exports.run = async (bot, message, args, fs) => {
   .setTitle(`${user.username}`)
   .setColor("#7d1616")
   .addField("ID:", `${user.id}`)
+  .addField("Roles:", `${user.roles}`)
+  .addField("Infractions:", `${warnlevel}`)
   .setImage(user.displayAvatarURL)
   message.delete().catch(O_o => {});
   message.channel.send({embed});
