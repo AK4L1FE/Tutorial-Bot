c9f2feeb2e5f42a86d531c40972448bbad8cdfec
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-14 20:25:31 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -15,7 +15,7 @@ module.exports = message => {
   }
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms, db.updateText);
+    cmd.run(client, message, params, perms, db);
   }
 
 };
