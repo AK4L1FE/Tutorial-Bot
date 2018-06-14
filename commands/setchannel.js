0b243034966ab62c89b675e04535a7f202fe560c
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-14 20:19:04 +0100

    Update setchannel.js

--- commands/setchannel.js
+++ commands/setchannel.js
@@ -3,11 +3,8 @@ const db = require('quick.db');
 
 exports.run = (bot, message, args, func) => {
 
-    // Return Statements
-    if (!message.member.roles.find('name', 'Owner')) return message.channel.send('**This command requires the Owner role**') // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
     if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return message.channel.send('**Please mention a channel**\n > *~setChannel #channel*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
 
-    // Fetch the new channel they mentioned
     let newChannel;
     if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; // If they wrote the word none, it sets newChannel as empty.
     else newChannel = message.mentions.channels.first().id; // If they actually mentioned a channel, it will set newChannel as that.
