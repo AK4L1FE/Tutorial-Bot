1aa9448e0246d4b1256d377413752f436f3cdd1f
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-14 20:05:57 +0100

    Create setwelcomechannel.js

---
+++ commands/setwelcomechannel.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+exports.run = (bot, message, args, func) => {
+
+    // Return Statements
+    if (!message.member.roles.find('name', 'Owner')) return func.embed(message.channel, '**This command requires the Owner role**', 120000) // This returns if it CANT find the owner role on them. It then uses the function to send to message.channel, and deletes the message after 120000 milliseconds (2minutes)
+    if (!message.mentions.channels.first() && args.join(" ").toUpperCase() !== 'NONE') return func.embed(message.channel, '**Please mention a channel**\n > *~setChannel #channel*') // This returns if they don't message a channel, but we also want it to continue running if they want to disable the log
+
+    // Fetch the new channel they mentioned
+    let newChannel;
+    if (args.join(" ").toUpperCase() === 'NONE') newChannel = ''; // If they wrote the word none, it sets newChannel as empty.
+    else newChannel = message.mentions.channels.first().id; // If they actually mentioned a channel, it will set newChannel as that.
+
+    // Update Channel
+    db.updateText(`messageChannel_${message.guild.id}`, newChannel).then(i => {
+        func.embed(message.channel, `**Successfully updated logging channel to ${message.mentions.channels.first()}**`) // Finally, send in chat that they updated the channel.
+    });
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'announce',
+  description: 'Mentions everyone and says what you type in.',
+  usage: 'announce {message}'
+}
