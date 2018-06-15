a91d9678d041490546c74cfe593b282f000b35c2
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-15 13:30:56 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
+  if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no. You don't have permissions for that.");
   if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <desired prefix here>");
 
   let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
