523f181de2bee139cdf542d38b8425c99c6134cc
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-15 13:10:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -75,3 +75,109 @@ client.on('error', e => {
 });
 
 client.login(settings.token);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+const bot = new Discord.Client();
+
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    const props = require(`./commands/${f}`);
+    log(`Loading Command: ${props.help.name}. 👌`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.on('message', async member => {
+
+    let prefixes =JSON.parse(fs.readFileSync('./prefixes.json', 'utf8'));
+
+    if(!prefixes[message.guild.id]) {
+        prefixes[message.guild.id] = {
+            prefixes: botconfig.prefix
+        };
+    }
+
+
+    let prefix = prefixes[message.guild.id].prefixes;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(settings.prefix)) return;
+    const command = message.content.split(' ')[0].slice(settings.prefix.length);
+    const params = message.content.split(' ').slice(1);
+    const perms = client.elevation(message);
+    let cmd;
+    if (client.commands.has(command)) {
+      cmd = client.commands.get(command);
+    } else if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    }
+    if (cmd) {
+      if (perms < cmd.conf.permLevel) return;
+      cmd.run(client, message, params, perms);
+    }
+  
+  });
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      const cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  const mod_role = message.member.hasPermission("ADMINISTRATOR");
+  if (mod_role) permlvl = 2;
+  const admin_role = message.member.hasPermission("ADMINISTRATOR");
+  if (admin_role) permlvl = 3;
+  if (message.author.id === settings.ownerid) permlvl = 4;
+  return permlvl;
+};
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(settings.token);
