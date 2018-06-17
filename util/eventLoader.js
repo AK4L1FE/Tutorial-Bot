45cf61f0380674adf92213785a2dbceeb2ad892c
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-17 22:26:52 +0100

    Update eventLoader.js

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,5 +3,4 @@ module.exports = async (client, message) => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('reconnecting', () => reqEvent('reconnecting')(client));
   client.on('disconnect', () => reqEvent('disconnect')(client));
-  client.on('message', reqEvent('message'));
 };
