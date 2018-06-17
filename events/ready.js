ae08563b44197ad4822e1b81d7ffb48d5c441900
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-17 12:00:10 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,10 @@ module.exports = async client => { // eslint-disable-line no-unused-vars
   setInterval(function () {
     client.user.setGame(`;help | ${client.users.size} Users | ${client.guilds.size} Servers`, 'https://www.twitch.tv/ak4l1fe'); 
     client.user.setGame(`;help | https://z-mod.glitch.me/`, 'https://www.twitch.tv/ak4l1fe');
+    client.user.setGame(`Join the Support Server!!!`, 'https://www.twitch.tv/ak4l1fe');
+    client.user.setGame(`If it gets 110 members, music commands!!!`, 'https://www.twitch.tv/ak4l1fe');
 //random status'es in here
-  }, [5000]);
+  }, [4000]);
  
   
 };
