8f5e6a4ccf81529a7942e63de0ad3e8c2b3c4de1
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-17 22:25:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,35 @@ fs.readdir('./commands/', (err, files) => {
     });
   });
 });
+client.on('message', async (message, guild, id) => {
+
+    let prefixes =JSON.parse(fs.readFileSync('./prefixes.json', 'utf8'));
+
+    if(!prefixes[message.guild.id]) {
+        prefixes[message.guild.id] = {
+            prefixes: settings.prefix
+        };
+    }
+
+
+    let prefix = prefixes[message.guild.id].prefixes;
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    const command = message.content.split(' ')[0].slice(prefix.length);
+    const params = message.content.split(' ').slice(1);
+    const perms = client.elevation(message);
+    let cmd;
+    if (client.commands.has(command)) {
+      cmd = client.commands.get(command);
+    } else if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    }
+    if (cmd) {
+      if (perms < cmd.conf.permLevel) return;
+      cmd.run(client, message, params, perms);
+    }
+  
+
 
 client.reload = command => {
   return new Promise((resolve, reject) => {
