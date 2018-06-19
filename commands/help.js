1a0a78078fb84283c133783e0ee7947a3385c324
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-19 12:26:15 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,8 @@ exports.run = (client, message, params) => {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}\nFor further help, please join The Z-Mod\'s Official Support Server for more help and suggestions: https://discord.gg/U3hbwrt`, {code:'asciidoc'});
+    message.react("458396576273137674");
+    message.react("458397350201917453");
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
