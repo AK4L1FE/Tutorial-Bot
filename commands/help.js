d3d0b0ebe7ab1d10127c63bec0380bb7b36dece9
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-06-19 12:16:39 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,10 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 const settings = require('../settings.json');
-const check = client.emojis.find("name", "gradcheckmark");
-const cross = client.emojis.find("name", "gradcrossmark");
 exports.run = (client, message, params) => {
   if (!params[0]) {
     const commandNames = Array.from(client.commands.keys());
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     message.channel.send(`= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${settings.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}`).join('\n')}\nFor further help, please join The Z-Mod\'s Official Support Server for more help and suggestions: https://discord.gg/U3hbwrt`, {code:'asciidoc'});
-    message.react(`${check}`, `${cross}`);
+    message.react(`:gradcheckmark:`, `:gradcrossmark:`);
   } else {
     let command = params[0];
     if (client.commands.has(command)) {
