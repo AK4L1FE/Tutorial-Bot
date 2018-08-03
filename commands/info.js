a30d8f1d4fe97cfedec8b46d746c4c3e5c127085
AK4L1FE <38872259+AK4L1FE@users.noreply.github.com>
2018-08-03 13:06:22 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = (clientUser, message, args) => {
-message.channel.send(`**Z-Mod**\n${clientUser.user.displayAvatarURL}\n\n**Version**\n1.0.2\n\n**Created at:**\n${clientUser.user.createdAt}\n\n**Library:**\ndiscord.js\n\n**Creator:**\n@AK4L1FE#4246\n\n**Invite to Support:**\nhttps://discord.gg/5gfntPY\n\n**Donate to my creator:**\nhttps://www.patreon.com/user?u=11509428`);
+message.channel.send(`**Z-Mod**\n${clientUser.user.displayAvatarURL}\n\n**Version**\n1.0.2\n\n**Created at:**\n${clientUser.user.createdAt}\n\n**Library:**\ndiscord.js\n\n**Creator:**\nSivveryl#4246\n\n**Invite to Support:**\nhttps://discord.gg/5gfntPY\n\n**Donate to my creator:**\nhttps://www.patreon.com/user?u=11509428`);
 };
 
 exports.conf = {
